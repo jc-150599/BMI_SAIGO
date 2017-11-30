5b80cf63c611410434a0a7eddc2294c04083ce53
駿太 境 <150599@jc-21.jp>
2017-11-30 16:30:41 +0900

    振り分けできた！

--- BMI_SAIGO/BMI_SAIGO/MainPage.xaml.cs
+++ BMI_SAIGO/BMI_SAIGO/MainPage.xaml.cs
@@ -17,8 +17,50 @@ namespace BMI_SAIGO
         private void BMI_Button_Clicked(object sender, EventArgs e)
         {
             double dHeight = double.Parse(height.Text);
+            double dWeight = double.Parse(weight.Text);
 
-            DisplayAlert("BMI", dHeight + "太りすぎ", "OK");
+            double dBMI = dWeight / dHeight / dHeight;
+
+            if (dHeight >= 3)
+            {
+                dHeight /= 100;
+            }
+
+
+
+            if (dBMI < 16.0)
+            {
+                DisplayAlert("BMI", "痩せすぎ", "OK");
+            }
+            else if (dBMI < 17.0)
+            {
+                DisplayAlert("BMI", "痩せ", "OK");
+            }
+            else if (dBMI < 19.0)
+            {
+                DisplayAlert("BMI", "痩せ気味", "OK");
+            }
+            else if (dBMI < 25.0)
+            {
+                DisplayAlert("BMI", "普通体重", "OK");
+            }
+            else if (dBMI < 30.0)
+            {
+                DisplayAlert("BMI", "前肥満", "OK");
+            }
+            else if (dBMI < 35.0)
+            {
+                DisplayAlert("BMI", "肥満（１度）", "OK");
+            }
+            else if (dBMI < 40.0)
+            {
+                DisplayAlert("BMI", "肥満（２度）", "OK");
+            }
+            else
+            {
+                DisplayAlert("BMI", "肥満（３度）", "OK");
+            }
+          
         }
     }
 }
